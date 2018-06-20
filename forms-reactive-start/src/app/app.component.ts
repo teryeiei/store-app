e6bfd2895d99d741fb48886f4c71d4b1aeebe0d6
DESKTOP-P23T5P2\NewTeryEiEi <bunyarit.run.it58@cpc.ac.th>
2018-06-20 17:58:05 +0700

    section forms-reactive

---
+++ forms-reactive-start/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+          'username': new FormControl(null, [Validators.required, this.forbiddenUsernames.bind(this)]),
+          'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+          'gender': new FormControl('male'),
+          'hobbies': new FormArray([])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  forbiddenName(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+      return null;
+  }
+}
+
